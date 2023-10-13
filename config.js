b02c38e4e0b5d42e1b27d135f73dc21f6648aacc
ilovejavascript369 <85027578+ilovejavascript369@users.noreply.github.com>
2023-10-13 10:35:37 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCJ_EUXWpXoQYy_UwWUPDFZWXkMJcuBn6I",
+    authDomain: "project85-39ff7.firebaseapp.com",
+    databaseURL: "https://project85-39ff7-default-rtdb.firebaseio.com",
+    projectId: "project85-39ff7",
+    storageBucket: "project85-39ff7.appspot.com",
+    messagingSenderId: "629990047683",
+    appId: "1:629990047683:web:bde16e3feca9aa8b4d4776"
+  };
